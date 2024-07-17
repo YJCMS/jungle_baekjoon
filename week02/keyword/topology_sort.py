7715b5b9153fc157f48be747d8d4a393d6b8c947
YJCMS <sen9563@gmail.com>
2024-07-17 15:07:59 +0900

    topology_sort

--- week02/keyword/topology_sort.py
+++ week02/keyword/topology_sort.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # 위상정렬 Kahn's Algorithm
 def topology_sort():
     result = []
@@ -18,3 +20,14 @@ def topology_sort():
  
     for i in result:
         print(i, end=' ')
+
+n, m = map(int, sys.stdin.readline().split())
+# 노드에 대한 진입차수를 담을 indegree를 0으로 초기화
+indegree = [0] * (n+1)
+graph = [[] for i in range(n+1)]
+
+# 방향그래프 간선정보 입력받기
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    indegree[b] += 1
